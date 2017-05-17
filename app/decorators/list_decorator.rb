c29996c3c920863e16ba13cb9df9f7bee2cb5b3f
Ataru Kodaka <ataru.kodaka@gmail.com>
2017-05-17 23:54:24 +0900

    decorator

---
+++ app/decorators/list_decorator.rb
@@ -0,0 +1,25 @@
+module ListDecorator
+  extend ActiveSupport::Concern
+
+  include ApplicationHelper
+
+  class_methods do
+    @@_filter_keys = []
+    
+    def set_filter_keys(*keys)
+      @@_filter_keys = keys
+
+      keys.each do |key|
+        self.send(:define_method, key) {
+          filter_index(key)
+        }
+      end
+    end
+  end
+  included do
+    delegate_all
+  end
+  def filter_index(key)
+    h.link_to_index(model[key], parameters: h.params.permit(@@_filter_keys).merge(key => model[key]))
+  end
+end
