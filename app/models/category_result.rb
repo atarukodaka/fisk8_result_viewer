1fd32e7377dab217a33fb4571763411166ef71cd
Ataru Kodaka <ataru.kodaka@gmail.com>
2017-05-21 02:01:27 +0900

    fix

--- app/models/category_result.rb
+++ app/models/category_result.rb
@@ -4,6 +4,7 @@ class CategoryResult < ApplicationRecord
 
   scope :search_by_category, ->(cat) { where(category: cat) }
 
+=begin
   def short
     Score.where(competition_id: competition.id, category: self.category).where("segment like ?", "SHORT%").first
   end
@@ -11,4 +12,5 @@ class CategoryResult < ApplicationRecord
   def free
     Score.where(competition_id: competition.id, category: self.category).where("segment like ?", "FREE%").first
   end
+=end
 end
