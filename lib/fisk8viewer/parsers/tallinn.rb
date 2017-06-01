8600dcf46b1a0f549596498593df104e93ab3a65
Ataru Kodaka <ataru.kodaka@gmail.com>
2017-06-01 18:33:28 +0900

    tallinn-trophy

--- lib/fisk8viewer/parsers/tallinn.rb
+++ lib/fisk8viewer/parsers/tallinn.rb
@@ -30,6 +30,7 @@ module Fisk8Viewer
               score_url: (score_url.present?) ? URI.join(@url, score_url).to_s : "",
             }
           end
+          binding.pry
           summary
         end
 
