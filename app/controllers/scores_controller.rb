ae5e8414c4acccbd713943d9598aec0ab1d32346
Ataru Kodaka <ataru.kodaka@gmail.com>
2017-06-03 12:17:55 +0900

    Revert "show-score-decorate"
    This reverts commit f2e53b3415f8b9b3c712e629ffca03d6c7cb457e.

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -7,43 +7,25 @@ class ScoreDecorator < EntryDecorator
   def sid
     h.link_to_score(model.sid, model)
   end
-  def skater
-    h.link_to_skater(model.skater)
-  end
-  def competition
-    h.link_to_competition(model.competition)
-  end
-  def category
-    h.link_to_competition(model.competition, category: model.category)
-  end
-  def segment
-    h.link_to_competition(model.competition, category: model.category, segment: model.segment)
-  end
-  def season
-    model.competition.season
-  end
   def result_pdf
     h.link_to_pdf(model.result_pdf)
-  end
-  def youtube_search
-    h.link_to("Youtube", "http://www.youtube.com/results?q=" + ERB::Util.html_escape([score.skater_name, score.competition_name, score.segment].join('+')), target: "_blank")
   end
 end
 ################################################################
 class ScoresController < ApplicationController
   def display_keys
-    [:sid, :competition, :category, :segment, :season, :date, :result_pdf,
-     :ranking, :skater, :nation, :tss, :tes, :pcs, :deductions, :base_value]
+    [:sid, :competition_name, :category, :segment, :season, :date, :result_pdf,
+     :ranking, :skater_name, :nation, :tss, :tes, :pcs, :deductions, :base_value]
   end
   def collection
-    Score.recent.includes(:competition, :skater).filter(filters.create_arel_tables(params))
+    Score.with_competition.recent.select("competitions.season, scores.*").filter(filters.create_arel_tables(params))
   end
   def show
-    score = Score.find_by(sid: params[:sid]) ||
+    score = Score.find_by(sid: params[:sid]).decorate ||
       raise(ActiveRecord::RecordNotFound.new("no such sid: '#{params[:sid]}'"))
 
     respond_to do |format|
-      format.html { render locals: {score: score.decorate}}
+      format.html { render locals: {score: score}}
       format.json { render json: {summary: score, elements: score.elements, components: score.components }}
     end
   end
