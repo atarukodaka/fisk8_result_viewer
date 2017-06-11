d08480c721fec78d6318302f5d5260c2cc32760f
Ataru Kodaka <ataru.kodaka@gmail.com>
2017-06-11 23:40:50 +0900

    home-controller-spec-added

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+  render_views
+
+  it 'home' do
+    get :index
+    expect(response.body).to include('Fisk8 Result Viewer')
+    expect(response.body).to include(Settings['about']['name'])
+  end
+    
+end
