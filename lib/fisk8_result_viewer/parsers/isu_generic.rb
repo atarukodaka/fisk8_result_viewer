55ffcac5bc024689b5639ca20ff7630af1c1916e
Ataru Kodaka <ataru.kodaka@gmail.com>
2017-06-15 15:44:21 +0900

    parser-updater-fix

--- lib/fisk8_result_viewer/parsers/isu_generic.rb
+++ lib/fisk8_result_viewer/parsers/isu_generic.rb
@@ -1,6 +1,13 @@
 module Fisk8ResultViewer
   module Parsers
     class IsuGeneric
+      class CompetitionParser < Fisk8ResultViewer::Competition::Parser
+      end
+      class CategoryResultParser < Fisk8ResultViewer::CategoryResult::Parser
+      end
+      class ScoreParser < Fisk8ResultViewer::Score::Parser
+      end
+=begin      
       module Competition
         class Parser < Fisk8ResultViewer::Competition::Parser
         end
@@ -13,6 +20,7 @@ module Fisk8ResultViewer
         class Parser < Fisk8ResultViewer::Score::Parser
         end
       end
+=end
       Fisk8ResultViewer::Parsers.register(:isu_generic, self)
     end
   end
