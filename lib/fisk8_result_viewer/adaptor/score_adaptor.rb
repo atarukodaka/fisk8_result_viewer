3de10b70e2aa3f40dd61e3e7f3b9a9013d8eb64e
Ataru Kodaka <ataru.kodaka@gmail.com>
2017-06-18 17:05:57 +0900

    fix-find-relevant-cr

--- lib/fisk8_result_viewer/adaptor/score_adaptor.rb
+++ lib/fisk8_result_viewer/adaptor/score_adaptor.rb
@@ -1,10 +1,16 @@
 module Fisk8ResultViewer
   module Adaptor
     class ScoreAdaptor
+      def find_relevant_category_result(category_results, skater_name, segment, ranking)
+        ranking_type = (segment =~ /^SHORT/) ? :short_ranking : :free_ranking
+        category_results.find_by(skater_name: skater_name) || 
+          category_results.where(ranking_type => ranking).first
+      end
+                                        
       def initialize(hash)
         score = ::Score.new(hash.except(:elements, :components))
         score.skater_name = ::Skater.correct_name(score.skater_name)
-        cr = score.competition.category_results.find_by(skater_name: score.skater_name) || raise
+        cr = find_relevant_category_result(score.competition.category_results, score.skater_name, hash[:segment], hash[:ranking]) ||  raise('cannot find relevant category results')
         score.skater = cr.skater
         score.skater_name = score.skater.name
 
