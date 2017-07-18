9a66342c616a79a2afa557ec3a213c4058dcc0a2
Ataru Kodaka <ataru.kodaka@gmail.com>
2017-07-19 01:00:31 +0900

    rm-serverside-module

--- app/datatables/datatable/decoratable.rb
+++ app/datatables/datatable/decoratable.rb
@@ -1,7 +1,14 @@
 module Datatable::Decoratable
   def decorate
-    add_manipulator(->(r){ r.decorate })
+    #add_manipulator(->(r){ r.decorate })
+    self.prepend Datatable::Decorate
     self
   end
 end  
+
+module Datatable::Decorate
+  def manipulate(r)
+    r.decorate
+  end
+end
 
