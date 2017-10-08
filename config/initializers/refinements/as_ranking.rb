8f490e22126def02816cd68bfcb7622331e77084
Ataru KODAKA <ataru.kodaka@gmail.com>
2017-10-08 12:55:22 +0100

    fix-as-

--- config/initializers/refinements/as_ranking.rb
+++ config/initializers/refinements/as_ranking.rb
@@ -1,4 +1,9 @@
 module AsRanking
+  refine Float do
+    def as_ranking
+      (self == 0) ? "-" : "%d" % [self]
+    end
+  end
   refine Integer do
     def as_ranking
       (self == 0) ? "-" : "%d" % [self]
