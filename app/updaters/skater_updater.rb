8be61cff76919c1fb5127b7a0daa7b9636c15fd7
Ataru KODAKA <ataru.kodaka@gmail.com>
2018-08-18 21:57:42 +0900

    fix-skater-updater

--- app/updaters/skater_updater.rb
+++ app/updaters/skater_updater.rb
@@ -4,7 +4,7 @@ class SkaterUpdater
     parser = SkaterParser.new
     ActiveRecord::Base.transaction do
       Category.all.select(&:isu_bio_url).each do |category|
-        binding.pry
+
         parser.parse_skaters(category.name, category.isu_bio_url).each do |hash|
           Skater.find_or_create_by(isu_number: hash[:isu_number]) do |skater|
             skater.update(hash)
