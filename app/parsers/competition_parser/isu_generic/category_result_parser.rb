ff9362a7326b8474650d3a96423001d60be68e61
Ataru KODAKA <ataru.kodaka@gmail.com>
2018-08-22 01:34:51 +0900

    fix

--- app/parsers/competition_parser/isu_generic/category_result_parser.rb
+++ app/parsers/competition_parser/isu_generic/category_result_parser.rb
@@ -1,7 +1,7 @@
 module CompetitionParser
   class IsuGeneric
-    class CategoryResultParser
-      include Utils
+    class CategoryResultParser < ResultParser
+
       def callbacks
         {
           to_i: lambda {|elem| elem.text.squish.to_i},
@@ -34,42 +34,6 @@ module CompetitionParser
         
         raise "No Placement Cell found (#{self.class})" if place_elem.nil?
         return place_elem.xpath("../../tr")
-      end
-
-      def get_headers(row)
-        elems = row.xpath("th").presence || row.xpath("td")
-        elems.map do |elem|
-          elem.text.squish.gsub(/[[:space:]]/, '')
-        end
-      end
-      ################
-      def parse(url)
-        puts "   --  parsing #{url}"
-        page = get_url(url, read_option: 'r:iso-8859-1').presence || (return [])
-        rows = get_rows(page)
-        headers = get_headers(rows[0])
-        ##
-        rows[1..-1].map do |row|
-          elems = row.xpath('td')
-          next if elems.size == 1
-
-          data = {}
-          columns.each do |key, params|
-            relevant_headers = [params[:header],].flatten
-
-            
-            col_number = headers.index {|d| relevant_headers.index(d)} ||
-                         raise("no relevant column found: #{key}: #{relevant_headers}")
-            elem = elems[col_number]
-            data[key] =
-              if (callback = params[:callback])
-                callback.call(elem)
-              else
-                elem.text.squish
-              end
-          end
-          data
-        end.compact  ## rows
       end
     end  ## class
   end
