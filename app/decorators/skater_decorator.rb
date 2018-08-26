9480cff651a6b802904029ead7e3e3924974c905
Ataru KODAKA <ataru.kodaka@gmail.com>
2018-08-27 01:11:59 +0900

    isu-number-with-recoad-link

--- app/decorators/skater_decorator.rb
+++ app/decorators/skater_decorator.rb
@@ -3,7 +3,22 @@ class SkaterDecorator < EntryDecorator
     h.link_to_skater(model)
   end
   def isu_number
-    h.link_to_isu_bio(model.isu_number)
+    h.content_tag(:span) do
+      h.concat(model.isu_number)
+      h.concat(" (")
+      h.concat(h.link_to_isu_bio("Biography", model.isu_number))
+      h.concat(' / ')
+      h.concat(h.link_to("Competition Results", "http://www.isuresults.com/bios/isufs_cr_%08d.htm" % [model.isu_number.to_i], target: :blank))
+      h.concat(h.span_link_icon())
+      h.concat(' / ')
+      h.concat(h.link_to("Personal Best", "http://www.isuresults.com/bios/isufs_pb_%08d.htm" % [model.isu_number.to_i], target: :blank))
+      h.concat(h.span_link_icon())
+      h.concat(")")
+    end
+
+
+  end
+  def isu_records
   end
 end
 
