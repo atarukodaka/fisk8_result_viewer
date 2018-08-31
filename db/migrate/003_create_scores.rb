95545172d5987abb88c683f97c0ac1eaeaab6f4d
Ataru KODAKA <ataru.kodaka@gmail.com>
2018-09-01 00:34:43 +0900

    gemfile-shape-up

--- db/migrate/003_create_scores.rb
+++ db/migrate/003_create_scores.rb
@@ -9,7 +9,6 @@ class CreateScores < ActiveRecord::Migration[5.1]
       t.string :segment
       t.string :segment_type
       t.date :date, default: Date.new(1970, 1, 1)
-      #t.datetime :segment_starting_time, default: Time.new(1970, 1, 1, 0, 0, 0)
       t.string :result_pdf
       
       t.float :tss, default: 0.0
@@ -25,7 +24,6 @@ class CreateScores < ActiveRecord::Migration[5.1]
       t.belongs_to :competition
       t.references :skater
       t.references :category_result
-      #t.references :performed_segment
     end
 
     create_table :elements do |t|
