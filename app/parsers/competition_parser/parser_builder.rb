0b95be422e9c9423bf725a33ac9f5487f1822b2a
Ataru KODAKA <ataru.kodaka@gmail.com>
2018-09-10 19:36:08 +0900

    fix

--- app/parsers/competition_parser/parser_builder.rb
+++ app/parsers/competition_parser/parser_builder.rb
@@ -2,7 +2,7 @@ class CompetitionParser
   class ParserBuilder
     class << self
       def build(parser_type = nil, verbose: false)
-        parser_type ||= DEFAULT_PARSER
+        parser_type ||= CompetitionParser::DEFAULT_PARSER
         parser_type_classname = parser_type.to_s.camelize
         parsers = {}
         [:summary, :category_result, :segment_result, :score].each do |subject|
