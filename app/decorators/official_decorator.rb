0f687dacddc48302c31d4c9678d97fafd2e24089
Ataru KODAKA <ataru.kodaka@gmail.com>
2018-09-23 11:25:24 +0900

    decorater

--- app/decorators/official_decorator.rb
+++ app/decorators/official_decorator.rb
@@ -6,9 +6,10 @@ class OfficialDecorator < EntryDecorator
   def category
     h.link_to_competition(model.performed_segment.competition, category: model.performed_segment.category)
   end
-
   def segment
     h.link_to_competition(model.performed_segment.competition, category: model.performed_segment.category, segment: model.performed_segment.segment)
   end
-
+  def panel_name
+    h.link_to_panel(model.panel)
+  end
 end
