404579a989fc0bdf1ad916317397b37a4267574c
Ataru KODAKA <ataru.kodaka@gmail.com>
2018-10-02 21:27:49 +0900

    fix

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -9,15 +9,11 @@ Capybara.register_driver :chrome do |app|
 end
 
 Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome(
     chromeOptions: { args: %w(headless disable-gpu window-size=1680,1050) }
   )
-
-  Capybara::Selenium::Driver.new app,
-    browser: :chrome,
-    desired_capabilities: capabilities
+  Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: caps)
 end
 
-
-Capybara.javascript_driver = (jsd = ENV['JAVASCRIPT_DRIVER']) ? jsd.to_sym : :headless_chrome
-
+#Capybara.javascript_driver = (jsd = ENV['JAVASCRIPT_DRIVER']) ? jsd.to_sym : :poltergeist
+Capybara.javascript_driver = ENV['JAVASCRIPT_DRIVER'].try(:to_sym) || :poltergeist
