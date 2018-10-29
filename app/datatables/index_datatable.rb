3cb931d5a9f331018f5cda68d2ef9f8a73e3a40b
Ataru KODAKA <ataru.kodaka@gmail.com>
2018-10-29 21:55:54 +0900

    fix

--- app/datatables/index_datatable.rb
+++ app/datatables/index_datatable.rb
@@ -68,7 +68,7 @@ class IndexDatatable < AjaxDatatables::Datatable
   end
 
   def default_settings
-    super.merge(pageLength: 25, searching: true, searching: false)
+    super.merge(pageLength: 25, searching: false)
   end
 
   def default_model
